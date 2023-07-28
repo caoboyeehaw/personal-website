9742fa59946b0a1b7fdf677c778af5ab756e87ee
caoboyeehaw <dylanhoangcao@gmail.com>
2023-07-27 20:31:42 -0500

    different email contact

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,7 +43,7 @@ export default function Ftr() {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="16 0 512 512" className="mt-0.5 w-5 h-5 text-white"><path d="M349.33 69.33a93.62 93.62 0 0193.34 93.34v186.66a93.62 93.62 0 01-93.34 93.34H162.67a93.62 93.62 0 01-93.34-93.34V162.67a93.62 93.62 0 0193.34-93.34h186.66m0-37.33H162.67C90.8 32 32 90.8 32 162.67v186.66C32 421.2 90.8 480 162.67 480h186.66C421.2 480 480 421.2 480 349.33V162.67C480 90.8 421.2 32 349.33 32z" fill="currentColor"></path> <path d="M377.33 162.67a28 28 0 1128-28 27.94 27.94 0 01-28 28zM256 181.33A74.67 74.67 0 11181.33 256 74.75 74.75 0 01256 181.33m0-37.33a112 112 0 10112 112 112 112 0 00-112-112z" fill="currentColor"></path></svg>
                     </a></div>
 
-                    <div title="Contact me via Email!"><a href="mailto:caoboycontactcao@gmail.com">
+                    <div title="Contact me via Email!"><a href="mailto:caoboyhoangcao@gmail.com">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="2 0 24 24" stroke="currentColor" className="mt-0.5 w-5 h-5 text-white"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path></svg>
                     </a></div>
                 </div>
